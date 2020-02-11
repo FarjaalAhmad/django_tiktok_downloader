9303afd34eae9db762d5c82486832a8de2328ca1
Farjaal Ahmad <farjaalahmad@gmail.com>
2020-02-11 17:00:03 +0500

    herokiu deployment

--- tiktok/settings.py
+++ tiktok/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["tiktokdownloaderw.herokuapp.com"]
 
 
 # Application definition
