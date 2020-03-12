eeb324e9f36e0de24cb6143386ee68213fad0763
Farjaal Ahmad <farjaalahmad@gmail.com>
2020-03-13 00:49:08 +0500

    passenger_wsgi + .htaccess added

--- tiktok/settings.py
+++ tiktok/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
